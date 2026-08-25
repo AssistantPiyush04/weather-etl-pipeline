3cac747ca1dbeb3dafd985d386185a3fe209b109
Piyush <piyushsharmarj14@gmail.com>
2026-08-25 15:54:48 +0530

    Updated Validate.py add some features

--- src/validate.py
+++ src/validate.py
@@ -1,17 +1,24 @@
 import mysql.connector
+import os
+
+from dotenv import load_dotenv
 from logger import logger
+
+load_dotenv()
 
 def validate_weather_data():
     logger.info("Data validation started")
 
     connection= mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Piyush@2004sql",
-        database="weather_etl"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     );
 
     cursor=connection.cursor(dictionary=True)
+
+    logger.info("connected to mysql succedfully")
 
     cursor.execute("""
       select * 
@@ -21,6 +28,28 @@ def validate_weather_data():
 
     records=cursor.fetchall()
 
+    null_records=0
+
+    for record in records:
+        if(
+            record["city"] is None 
+           or record["temperature"] is None
+           or record["humidity"] is None
+           or record["wind_speed"] is None
+           or record["recorded_at"] is None
+
+        ):
+           null_records+=1
+        
+    if null_records == 0:
+        print("Null: check pass")
+        logger.info("Null checked passes")
+
+    else:
+        print(f"Null check Fail- {null_records} records contain null values")
+        logger.warning(f"Null checked failed- {null_records} records contain null values")
+
+    print(f"Total records fetched: {len(records)}")
     logger.info(f"{len(records)} records fetched for validation")
 
     cursor.close()
